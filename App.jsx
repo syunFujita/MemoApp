6dcfd58a90719bcb3a97f0a7d5a6c5c5c13f550d
fujita <15869669+syunFujita@users.noreply.github.com>
2021-06-15 21:48:37 +0900

    Make components touchable

--- App.jsx
+++ App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-// import MemoListScreen from './src/screens/MemoListScreen';
+import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
-import SignUpScreen from './src/screens/SignUpScreen';
+// import SignUpScreen from './src/screens/SignUpScreen';
 // import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
   return (
-    <SignUpScreen />
+    <MemoListScreen />
   );
 }
